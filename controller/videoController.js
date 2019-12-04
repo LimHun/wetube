3e1ecdfdee9402703682bc00c702916c5a45d410
Tunko <dlagnskkld3@naver.com>
2019-12-04 19:03:02 +0900

    2.25 Complite

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,4 +1,5 @@
 import { videos } from "../db"
+import routes from "../routes"
 
 export const home = (req, res) => {
   res.render("home", { pageTitle : "Home", videos})
@@ -11,14 +12,23 @@ export const search = (req, res) => {
     res.render("search", { pageTitle : "search", searchingBy, videos })
 }
 
-export const upload = (req, res) =>
-  res.render("upload", { pageTitle: "Upload" });
+export const getUpload = (req, res) => {  
+    res.render("upload", { pageTitle: "Upload" });
+}
+
+export const postUpload = (req, res) => {
+    const {
+        body: {file, title, description}
+    } = req
+
+    res.redirect(routes.videoDetail(324393))
+}
 
 export const videoDetail = (req, res) =>
-  res.render("videoDetail", { pageTitle: "Video Detail" });
+    res.render("videoDetail", { pageTitle: "Video Detail" });
 
 export const editVideo = (req, res) =>
-  res.render("editVideo", { pageTitle: "Edit Video" });
+    res.render("editVideo", { pageTitle: "Edit Video" });
 
 export const deleteVideo = (req, res) =>
-  res.render("deleteVideo", { pageTitle: "Delete Video" });
+    res.render("deleteVideo", { pageTitle: "Delete Video" });
